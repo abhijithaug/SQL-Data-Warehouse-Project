c69ad100d0e1582243436f24e9bed76db01ac10b
ABHIJITH AUGUSTINE <137610807+abhijithaug@users.noreply.github.com>
2025-03-12 12:33:58 +1000

    Update ini_database.sql

--- scripts/ini_database.sql
+++ scripts/ini_database.sql
@@ -1,9 +1,19 @@
-USE MASTER;
+/*
+=============================================
+Create Database and Schemas
+=============================================
 
+This script creates a new database and checks if it already exists.
+The script setup three schemas within the database: bronze, silver and gold
+
+
+USE MASTER;
+GO
 CREATE DATABASE DataWarehouse27022025;
 
 USE DataWarehouse27022025;
 
+--Create schemas
 CREATE SCHEMA bronze;
 GO
 CREATE SCHEMA Silver;
